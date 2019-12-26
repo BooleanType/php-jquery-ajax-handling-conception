c49463261674db219dc0647f531e36e84028b634
BooleanType <booleantype@ya.ru>
2019-12-26 18:27:20 +0100

    Normalize index.php

--- demo/index.php
+++ demo/index.php
@@ -1,1 +1,35 @@
-<?php// Include some functions for handling possible errors.include_once 'helper.php';try {    if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {        throw new Exception(json_encode([403, 'Forbidden']));    }    // Here is your custom code.    // $client_data processing below is just for example.    if (!isset($_POST['client_data'])) {        throw new Exception(json_encode([400, 'Bad request']));    }        $clientData = (int) $_POST['client_data'];    if ($clientData == 0) {        $errData = [            'client_data' => [                0 => 'Error description.',                1 => 'Another error description.',            ]        ];        throw new Exception(errMsg(500, $errData));    } else {        $requestedData = [            'Requested Data',            'More Requested Data'        ];        die(json_encode([200, json_encode($requestedData)]));    }} catch (Exception $e) {    die($e->getMessage());}
+<?php
+// Include some functions for handling possible errors.
+include_once 'helper.php';
+
+try {
+    if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        throw new Exception(json_encode([403, 'Forbidden']));
+    }
+
+    // Here is your custom code.
+    // $client_data processing below is just for example.
+    if (!isset($_POST['client_data'])) {
+        throw new Exception(json_encode([400, 'Bad request']));
+    }
+    
+    $clientData = (int) $_POST['client_data'];
+
+    if ($clientData == 0) {
+        $errData = [
+            'client_data' => [
+                0 => 'Error description.',
+                1 => 'Another error description.',
+            ]
+        ];
+        throw new Exception(errMsg(500, $errData));
+    } else {
+        $requestedData = [
+            'Requested Data',
+            'More Requested Data'
+        ];
+        die(json_encode([200, json_encode($requestedData)]));
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
+}
